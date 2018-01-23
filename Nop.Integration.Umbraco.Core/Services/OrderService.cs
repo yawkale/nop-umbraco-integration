46d99701abcf2867043994fa2fced2c533258990
Yevhenii_Koliensik <yevhenii.koliesnik@uteam.co>
2018-01-23 12:35:17 +0200

    add changes UI for paypal integra

---
+++ Nop.Integration.Umbraco.Core/Services/OrderService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Nop.Integration.Umbraco.Nop;
+using Nop.Integration.Umbraco.Orders;
+
+namespace Nop.Integration.Umbraco.Core.Services
+{
+    public class OrderService
+    {
+        private readonly NopApiService _nopService;
+        private readonly UserContext _userContext;
+
+
+        public OrderService()
+        {
+            _nopService = new NopApiService();
+            _userContext = new UserContext();
+        }
+
+        public List<Orders.Order> GetOrders()
+        {
+            var orders = _nopService.GetAllOrders();
+
+            return orders;
+        }
+
+        public string CreateOrder()
+        {
+
+            int customerIdParseResult;
+            int.TryParse(_userContext.CustomerId(), out customerIdParseResult);
+            var orderItems = new List<OrderItem> { new OrderItem() { Quantity = 2, ProductId = 38 }, new OrderItem() { Quantity = 1, ProductId = 2 } };
+            var shippingAddress = new Address
+            {
+                Address1 = "21 West 52nd Street",
+                Email = "alex@uteam.co.il",
+                FirstName = "John",
+                LastName = "Smith",
+                City = "New York",
+                PhoneNumber = "12345678",
+                ZipPostalCode = "10021",
+                CountryId = 1,
+                CreatedOnUtc = DateTime.UtcNow
+            };
+            var billingAddress = new Address
+            {
+                Address1 = "21 West 52nd Street",
+                Email = "alex@uteam.co.il",
+                FirstName = "John",
+                LastName = "Smith",
+                City = "New York",
+                PhoneNumber = "12345678",
+                ZipPostalCode = "10021",
+                CountryId = 1,
+                CreatedOnUtc = DateTime.UtcNow,
+
+            };
+
+
+            var stubOrder = new Orders.Order
+            {
+                CustomerId = customerIdParseResult,
+                OrderItems = orderItems,
+                ShippingMethod = "Shipping.FixedRate",
+                ShippingRateComputationMethodSystemName = " Shipping Rate Computation Method System Name",
+                ShippingAddress = shippingAddress,
+                BillingAddress = billingAddress,
+                CreatedOnUtc = DateTime.UtcNow,
+                PaidDateUtc = DateTime.UtcNow.ToString(CultureInfo.InvariantCulture),
+                PaymentMethodSystemName = "Payments.Manual"
+
+            };
+
+            var order = _nopService.CreateOrder(stubOrder);
+            return order.Id;
+        }
+    }
+}
---
+++ NopStarterKit.Web/Controllers/CheckoutController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Nop.Integration.Umbraco.Core.Services;
+using Umbraco.Web.Mvc;
+
+namespace NopStarterKit.Web.Controllers
+{
+    public class CheckoutController : SurfaceController
+    {
+        private OrderService orderService;
+
+        public CheckoutController()
+        {
+            this.orderService = new OrderService();
+        }
+
+        // GET: Checkout
+        public ActionResult ConfirmOrder()
+        {
+            var orderId = orderService.CreateOrder();
+            return RedirectToAction("PayPalCreateRequest", "PayPal");
+        }
+    }
+}
